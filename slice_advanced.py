e50f71948fd710fd62810b598c8983a5e33e1c1c
Cuchu Seré <69125170+Cuchu59@users.noreply.github.com>
2024-04-05 11:39:41 -0400

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,6 +1,6 @@
 def slice_advanced():
     texto = input()
-    print(texto[4::2]).
+    print(texto[4::2])
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
